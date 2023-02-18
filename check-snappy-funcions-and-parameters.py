c37ae6d0951ab5c3e75e62566dca2e17bd31c2fd
eupassarinho <erlispinto@outlook.com>
2023-02-18 11:32:34 -0300

    creating script for checking snappy functions

---
+++ check-snappy-funcions-and-parameters.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+
+Use this to check snappy funcions' parameters.
+
+Created on Sat Feb 18, 2023
+Last updated on: Sat Feb 18, 2023
+
+This code is part of the Erli's Ph.D. thesis
+
+Author: Erli Pinto dos Santos
+Contact-me on: erlipinto@gmail.com or erli.santos@ufv.br
+
+"""
+
+#%% REQUESTED MODULE
+
+# snappy module to create products:
+from snappy import GPF
+
+#%% LOADING GPT: USE THIS TO GET THE OPERATORS' PARAMETERS
+
+def listParams(operator_name):
+    GPF.getDefaultInstance().getOperatorSpiRegistry().loadOperatorSpis()
+    op_spi = GPF.getDefaultInstance().getOperatorSpiRegistry().getOperatorSpi(operator_name)
+    print('Op name:', op_spi.getOperatorDescriptor().getName())
+    print('Op alias:', op_spi.getOperatorDescriptor().getAlias())
+    param_Desc = op_spi.getOperatorDescriptor().getParameterDescriptors()
+    for param in param_Desc:
+        print(param.getName(), "or", param.getAlias())
+
+listParams('Terrain-Correction')
+
+import subprocess
+
+print(subprocess.Popen(['gpt', '-h', 'Terrain-Correction'],
+                       stdout = subprocess.PIPE,
+                       universal_newlines = True).communicate()[0])
